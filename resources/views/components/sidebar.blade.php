411a013ec820d31dbbe4a27c4bbb1e3aa9fd9464
akhdanrgya <akhdan.anargya@gmail.com>
2025-05-22 17:54:07 +0700

    update

--- resources/views/components/sidebar.blade.php
+++ resources/views/components/sidebar.blade.php
@@ -1,42 +1,41 @@
-
 <div class="fixed top-0 left-0 h-screen w-[198px] bg-white flex flex-col gap-7 px-6 py-2.5 overflow-y-auto z-50">
   <img src="/images/sidebar-logo.png" class="self-stretch flex-grow-0 flex-shrink-0 h-[73.94px] object-none" />
 
   <p class="text-xs text-[#4c5a73]">OVERVIEW</p>
 
-  <a href="{{ route('dashboard') }}" class="flex items-center gap-2.5 cursor-pointer">
+  <a href="{{ route('dashboard') }}" class="flex items-center gap-2.5 cursor-pointer px-2 py-1 rounded hover:bg-[#f3f4f6]">
     <i class="fas fa-home text-[#4c5a73] w-4"></i>
     <p class="text-sm text-[#4c5a73]">Home</p>
   </a>
 
-  <a href="{{ route('dashboard.courses') }}" class="flex items-center gap-2.5 cursor-pointer">
+  <a href="{{ route('dashboard.courses') }}" class="flex items-center gap-2.5 cursor-pointer px-2 py-1 rounded hover:bg-[#f3f4f6]">
     <i class="fas fa-chalkboard-user text-[#4c5a73] w-4"></i>
     <p class="text-sm text-[#4c5a73]">Courses</p>
   </a>
 
-  <a href="{{ route('dashboard.task') }}" class="flex items-center gap-2.5 cursor-pointer">
+  <a href="{{ route('dashboard.task') }}" class="flex items-center gap-2.5 cursor-pointer px-2 py-1 rounded hover:bg-[#f3f4f6]">
     <i class="fas fa-list-check text-[#4c5a73] w-4"></i>
     <p class="text-sm text-[#4c5a73]">Task</p>
   </a>
 
-  <a href="" class="flex items-center gap-2.5 cursor-pointer">
+  <a href="{{ route('dashboard.forum') }}" class="flex items-center gap-2.5 cursor-pointer px-2 py-1 rounded hover:bg-[#f3f4f6]">
     <i class="fas fa-users text-[#4c5a73] w-4"></i>
     <p class="text-sm text-[#4c5a73]">Forum</p>
   </a>
 
   <p class="text-xs text-[#4c5a73] mt-4">FRIENDS</p>
 
-  <a href="" class="flex items-center gap-2.5 cursor-pointer">
+  <a href="" class="flex items-center gap-2.5 cursor-pointer px-2 py-1 rounded hover:bg-[#f3f4f6]">
     <i class="fas fa-user-ninja text-[#4c5a73] w-4"></i>
     <p class="text-sm text-[#4c5a73]">Martha</p>
   </a>
 
-  <a href="" class="flex items-center gap-2.5 cursor-pointer">
+  <a href="" class="flex items-center gap-2.5 cursor-pointer px-2 py-1 rounded hover:bg-[#f3f4f6]">
     <i class="fas fa-user-astronaut text-[#4c5a73] w-4"></i>
     <p class="text-sm text-[#4c5a73]">Adan</p>
   </a>
 
-  <a href="" class="flex items-center gap-2.5 cursor-pointer">
+  <a href="" class="flex items-center gap-2.5 cursor-pointer px-2 py-1 rounded hover:bg-[#f3f4f6]">
     <i class="fas fa-skull text-[#4c5a73] w-4"></i>
     <p class="text-sm text-[#4c5a73]">Nca</p>
   </a>
