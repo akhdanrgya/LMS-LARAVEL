3b63902736ce3985c7cd218243ae559193679071
akhdanrgya <akhdan.anargya@gmail.com>
2025-06-05 16:09:22 +0700

    update user seeder

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,59 +21,6 @@ class UserSeeder extends Seeder
             'email_verified_at' => now(),
         ]);
         // Admin biasanya gak punya profil mentor/student khusus, tapi kalo mau bisa ditambahin
-
-        // 2. Buat beberapa Mentor
-        $mentor1 = User::create([
-            'name' => 'Akhdan',
-            'email' => 'akhdan@gmail.com',
-            'password' => Hash::make('akhdan123'),
-            'role' => 'mentor',
-            'email_verified_at' => now(),
-        ]);
-        MentorProfile::create([
-            'user_id' => $mentor1->id,
-            'bio' => 'Pengajar berpengalaman di bidang Web Development.',
-            'expertise' => 'PHP, Laravel, JavaScript',
-            'experience_years' => 5,
-        ]);
-
-        $mentor2 = User::create([
-            'name' => 'Arvia',
-            'email' => 'arvia@gmail.com',
-            'password' => Hash::make('arvia123'),
-            'role' => 'mentor',
-            'email_verified_at' => now(),
-        ]);
-        MentorProfile::create([
-            'user_id' => $mentor2->id,
-            'bio' => 'Praktisi Digital Marketing dengan fokus di SEO dan Content.',
-            'expertise' => 'Digital Marketing, SEO, Content Writing',
-        ]);
-
-        // 3. Buat beberapa Student
-        $student1 = User::create([
-            'name' => 'Enrico',
-            'email' => 'enrico@gmail.com',
-            'password' => Hash::make('enrico123'),
-            'role' => 'student',
-            'email_verified_at' => now(),
-        ]);
-        StudentProfile::create([
-            'student_id' => $student1->id,
-            // total_score dan level bisa default dari migrasi
-        ]);
-
-        $student2 = User::create([
-            'name' => 'Nadya',
-            'email' => 'nadya@gmail.com',
-            'password' => Hash::make('nadya123'),
-            'role' => 'student',
-            'email_verified_at' => now(),
-        ]);
-        StudentProfile::create([
-            'student_id' => $student2->id,
-        ]);
-
         // Lo bisa tambahin lebih banyak user pake looping atau User Factory
         // Contoh pake User Factory (kalo udah dibikin):
         // User::factory(10)->create()->each(function ($user) {
